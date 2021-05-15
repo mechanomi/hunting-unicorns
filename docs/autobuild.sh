323c365801500f39dad85bef2acb5f0d4428e2eb
Naomi Slater <23469+mechanomi@users.noreply.github.com>
2021-05-15 16:17:35 +0200

    Add `autobuild.sh` script and docs, plus minor fixes
    Specifically:
    
    - Adds a build script that mirrors the autobuild instructions in the
      `README.md` file
    
    - Adds a new *Build script* section to `README.md` to document this script
    
    - Fixes Sphinx version numbers in examples and the use of a hyphen

---
+++ docs/autobuild.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -e
+
+# This build script mirrors the autobuild instructions in the `README.md` file
+
+PY="python3"
+
+if test ! -d _venv; then
+	"${PY}" -m venv _venv
+	"_venv/bin/${PY}" -m pip install --upgrade pip
+	"_venv/bin/${PY}" -m pip install -r docs/requirements.txt
+	"_venv/bin/${PY}" -m pip install sphinx-autobuild
+fi
+
+_venv/bin/sphinx-autobuild --open-browser --delay 1 -b html . _out
